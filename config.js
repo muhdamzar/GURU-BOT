862812dbda097aa23edd535eee8c78dbfd8d37af
muhdamzar <152429407+muhdamzar@users.noreply.github.com>
2023-11-30 03:14:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,16 @@ import axios from 'axios'
 
 global.owner = [
   ['60104348321', 'UniX', true],
-  ['60105707500', 'Amzar', true], 
+  ['60105707500', 'Amzar', true],
+  ['66617077633', 'ThaiBoT', true],
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['60104348321','32460248586'] 
-global.prems = ['60104348321', '32460248586', '919398758484']
-global.allowed = ['60104348321', '32460248586', '19152999993']
+global.mods = ['60104348321', '60105707500', '66617077633'] 
+global.prems = ['60104348321', '60105707500', '66617077633']
+global.allowed = ['60104348321', '60105707500', '66617077633']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
